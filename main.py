7ce3cfb36d28151d1acbcdb90b4df0c482795ec8
piavik <piavik@gmail.com>
2023-12-25 21:21:38 +0100

    initial placeholders

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from datetime import datetime
+from random import randint, choice
+import sqlite3
+import faker
+
+# from create_db import create_db
+# from fill_data import generate_fake_data
+
+NUMBER_OF_STUDENTS = 50
+NUMBER_OF_GROUPS = 3
+NUMBER_OF_SUBJECTS = 8
+NUMBER_OF_LECTORS = 5
+NUMBER_OF_SCORES = 20
+
+# db_name
+# create_sql = 
+
+def prepare_data():
+    ...
+
+def insert_data_to_db(companies, employees, payments) -> None:
+    ...
+
+def main() -> None:
+    ...
+
+
+
+if __name__ == "__main__":
+    main()
