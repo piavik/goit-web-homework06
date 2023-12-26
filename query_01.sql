186fe1ecbaec4e260757e28a83f2bd4efd6fa21e
piavik <piavik@gmail.com>
2023-12-26 01:38:42 +0100

    query_01

--- query_01.sql
+++ query_01.sql
@@ -1,1 +1,10 @@
 --- Знайти 5 студентів із найбільшим середнім балом з усіх предметів.
+SELECT  student_name, score
+FROM    students 
+LEFT JOIN scores ON students.id = scores.student_id
+WHERE   score = (
+    SELECT max(score)
+    FROM scores
+)
+ORDER BY student_name
+LIMIT   5;
